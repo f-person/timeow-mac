38c18f3935fdf0fa7e1e5925bd38dd2c61927164
f-person <agakaryan.arshak@gmail.com>
2021-07-03 19:02:15 +0400

    refactor: move addBreakEntry to break.go

--- break.go
+++ break.go
@@ -37,3 +37,42 @@ func (b *breakEntry) string() string {
 		durafmt.Parse(duration).LimitFirstN(limit).String(),
 	)
 }
+
+func (a *app) addBreakEntry(start, end time.Time) {
+	entry := breakEntry{
+		start: start,
+		end:   end,
+	}
+
+	// Avoid duplicate breaks.
+	length := len(a.breaks)
+	if length > 0 {
+		lastBreak := a.breaks[length-1]
+		if entry.start.Truncate(time.Minute) == lastBreak.start.Truncate(time.Minute) {
+			if entry.end.After(lastBreak.end) {
+				a.breaks[length-1] = entry
+			}
+		} else {
+			a.breaks = append(a.breaks, entry)
+		}
+	} else {
+		a.breaks = append(a.breaks, entry)
+	}
+
+	fmt.Println("----------")
+	fmt.Printf("New break added: start = %v, end =%v , duration = %v.\n", entry.start, entry.end, entry.duration())
+	fmt.Printf("string: %v\n", entry.string())
+	fmt.Println("current time:", time.Now())
+	fmt.Println("----------")
+
+	if len(a.breakMenuItems) == len(a.breaks)-1 {
+		item := a.mBreaks.AddSubMenuItem("", "")
+		item.Disable()
+		a.breakMenuItems = append(a.breakMenuItems, item)
+	}
+
+	length = len(a.breaks)
+	for index, entry := range a.breaks {
+		a.breakMenuItems[length-index-1].SetTitle(entry.string())
+	}
+}
