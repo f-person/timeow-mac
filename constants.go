36c38538674fb4e16cd5ba2c82ca28debb12eb6d
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-03-24 03:23:48 +0400

    chore: change about url

--- constants.go
+++ constants.go
@@ -38,4 +38,4 @@ var keepTimeLogsForOptionsInSettings = [...]uint32{
 }
 
 const getProURL = "https://github.com/f-person"
-const aboutURL = "https://timeow.com/?ref=about-button"
+const aboutURL = "https://github.com/f-person/timeow-mac"
